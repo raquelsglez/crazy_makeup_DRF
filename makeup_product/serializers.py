deaa9adda33331fd88adf8680dcbe358228a6740
Raquel <raquelsglez99@gmail.com>
2023-05-18 15:44:14 +0200

    feature: better_code

--- makeup_product/serializers.py
+++ makeup_product/serializers.py
@@ -10,3 +10,10 @@ class MakeupProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = MakeupProduct
         fields = "__all__"
+
+
+class UpdateMakeupProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MakeupProduct
+        fields = "__all__"
+        read_only_fields = ["id", "color", "trademark"]
