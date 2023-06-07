fc64838d1a2e963a68aecf95c56f4fb0860491fa
Raquel <raquelsglez99@gmail.com>
2023-06-07 19:23:39 +0200

    feature: filters

--- crazymakeup/settings.py
+++ crazymakeup/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     'rest_framework_simplejwt',
+    'django_filters',
     'makeup_product',
     'authentication',
     'exercises'
@@ -149,6 +150,10 @@ REST_FRAMEWORK = {
 
    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+
+   'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 
    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
--- exercises/views.py
+++ exercises/views.py
@@ -1,4 +1,5 @@
 # Django and DRF imports
+import django_filters
 from rest_framework import mixins
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -12,7 +13,13 @@ class TelevisionViewSet(ModelViewSet):
     queryset = Television.objects.all()
     serializer_class = TelevisionSerializer
     lookup_field = 'numero_de_serie'
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [SearchFilter, OrderingFilter, django_filters.rest_framework.DjangoFilterBackend]
+
+    filterset_fields = {
+        "pulgadas": ["lt", "lte", "exact", "gte", "gt", "in"],
+        "numero_de_serie": ["in", "icontains", "exact"]
+    }
+
     search_fields = ['pulgadas']
     ordering_fields = "__all__"
 
@@ -21,6 +28,15 @@ class NeveraViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Updat
     queryset = Nevera.objects.all()
     serializer_class = NeveraSerializer
     lookup_field = 'id'
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [SearchFilter, OrderingFilter, django_filters.rest_framework.DjangoFilterBackend]
+
+    filterset_fields = {
+        "marca": ["icontains", "isnull", "exact", "in"],
+        "altura": ["lt", "lte", "exact", "gte", "gt", "in"],
+        "nombre": ["icontains"],
+        "color": ["icontains", "exact", "in"],
+        "anchura": ["lt", "lte", "exact", "gte", "gt", "in"]
+    }
+
     search_fields = ['color', 'nombre', 'marca']
     ordering_fields = "__all__"
