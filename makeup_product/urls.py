b773aa12aeae51b7d1c9726dd248420cabb930b7
Raquel <raquelsglez99@gmail.com>
2023-06-20 13:46:33 +0200

    feature: me_views

--- makeup_product/urls.py
+++ makeup_product/urls.py
@@ -2,10 +2,12 @@
 from rest_framework.routers import SimpleRouter
 
 # proof class import
-from .views import MakeupProductViewSet
+from .views import MakeupProductViewSet, MeMakeupProductView
 
 router = SimpleRouter()  # crea rutas
 
 router.register(r'makeup-products', MakeupProductViewSet, basename="makeup_products")
+router.register(r'me/makeup-products', MeMakeupProductView, basename="me_makeup_products")
+
 
 urlpatterns = router.urls
--- makeup_product/views.py
+++ makeup_product/views.py
@@ -1,7 +1,6 @@
 # Django and DRF imports
 import django_filters
 from rest_framework import status, mixins
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import GenericViewSet
@@ -34,12 +33,6 @@ class MakeupProductViewSet(mixins.CreateModelMixin,
     search_fields = ['name', 'color', 'trademark']
     ordering_fields = ['name', 'color', 'trademark', 'price']
 
-    def get_queryset(self):
-        queryset = self.queryset
-        if self.action in ("me_list", "me"):
-            queryset = queryset.filter(user=self.request.user.id)
-        return queryset
-
     def create(self, request, *args, **kwargs):
         request.data['user'] = request.user.id
         serializer = self.get_serializer(data=request.data)
@@ -48,40 +41,23 @@ class MakeupProductViewSet(mixins.CreateModelMixin,
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    @action(detail=True, methods=['PUT', 'PATCH', 'DELETE'])
-    def me(self, request, id):
-
-        if request.method in ('PUT', 'PATCH'):
-            instance = self.get_object()
-            serializer = self.get_serializer(instance, data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
-
-        elif request.method == 'DELETE':
-            instance = self.get_object()
-            instance.delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(detail=False, methods=['GET'], url_path="me-list", url_name="me-list")
-    def me_list(self, request):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
     def get_serializer_class(self):
         if self.action == "create":
             return CreateMakeupProductSerializer
-        elif self.action == 'me':
-            return UpdateMakeupProductSerializer
-        elif self.action == 'me_list':
-            return ListProfileMakeupProductSerializer
+        return self.serializer_class
 
+
+class MeMakeupProductView(mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, GenericViewSet):
+
+    queryset = MakeupProduct.objects.all()
+    serializer_class = ListProfileMakeupProductSerializer
+    lookup_field = "id"
+
+    def get_queryset(self):
+        queryset = self.queryset.filter(user=self.request.user)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ("update", "partial_update"):
+            return UpdateMakeupProductSerializer
         return self.serializer_class
